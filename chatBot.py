9cfc291f3233452c243c1b86cc0964cc253aa759
Nidhi Trapasiya <51698145+NidhiTrapasiya@users.noreply.github.com>
2019-06-20 19:00:49 +0530

    Add files via upload
    I have used pythan version 3.7
    This is very simple chat bot where fixed question are asked but it will give different answer.
    ask question like what is your name?
    what's today's weather?

---
+++ chatBot.py
@@ -0,0 +1,37 @@
+# Import the random module
+import random
+
+# Define variables 
+name = "Greg"   
+weather = "cloudy"
+flag=1
+
+# Define a dictionary containing a list of responses for each message
+#str.formate(str) used for substituting 
+responses = {
+  "what is your name?": [
+      "my name is {0}".format(name),
+      "they call me {0}".format(name),
+      "I go by {0}".format(name)
+   ],
+  "what's today's weather?": [
+      "the weather is {0}".format(weather),
+      "it's {0} today".format(weather)
+    ],
+  "default": ["default message"]
+}
+
+# Use random.choice() to choose a matching response
+def respond(message):
+    if message in responses:
+        bot_message = random.choice(responses[message])
+    else:
+        bot_message = random.choice(responses["default"])
+    return bot_message
+while(flag):
+    question=input("write your question......or write Q to Quit....")
+    if question=="Q":
+        flag=0
+    else:
+        print(respond(question))
+    
